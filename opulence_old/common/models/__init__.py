2f9ff2a0ac39e5450ac608fd5b5a0ad81e5751f0
jurelou <louis.jurczyk@epitech.eu>
2021-04-06 20:47:16 +0200

    rm old_opu

--- opulence_old/common/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from .case import Case
-from .scan import Scan, ScanConfig, ScanState, ScanResult
-from .fact import BaseFact
--- opulence_old/common/singleton.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-class Singleton:
-    _instance = None
-
-    def __new__(class_, *args, **kwargs):
-        if not isinstance(class_._instance, class_):
-            class_._instance = object.__new__(class_, *args, **kwargs)
-        return class_._instance
